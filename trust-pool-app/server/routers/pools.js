256351785d8042aad8ee4f1ca89d8618c0650e98
Jelani <jhankins02@gmail.com>
2018-06-25 13:49:03 -0500

    (add) post to /pools

--- trust-pool-app/server/routers/pools.js
+++ trust-pool-app/server/routers/pools.js
@@ -5,5 +5,8 @@ const pools = express.Router();
 pools.get('/', (req, res) => {
   res.status(200).send('sent get request to pools');
 });
+pools.post('/', (req, res) => {
+  res.status(200).send('sent post request to pools');
+});
 
 module.exports = pools;
