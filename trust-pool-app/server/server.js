cdbca143456567fefaf4597f169b70bcd6613590
Jelani <jhankins02@gmail.com>
2018-06-25 12:22:21 -0500

    (add) server use cors

--- trust-pool-app/server/server.js
+++ trust-pool-app/server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 
 const app = express();
 const server = http.Server(app);
+app.use(cors());
 
 module.exports.server = server;
