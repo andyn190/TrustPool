5e7810772d54369c02363df4fd113e80e28c07a3
Jelani <jhankins02@gmail.com>
2018-06-25 13:49:20 -0500

    (add) signup router to routers index

--- trust-pool-app/server/routers/index.js
+++ trust-pool-app/server/routers/index.js
@@ -1,6 +1,8 @@
 const poolsRouter = require('./pools');
+const signupRouter = require('./signup');
 
 
 module.exports = (app) => {
   app.use('/pools', poolsRouter);
+  app.use('/signup', signupRouter);
 };
