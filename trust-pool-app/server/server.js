e44d3dc5776d55167f878b588576f3d2c3f56578
Jelani <jhankins02@gmail.com>
2018-06-25 12:23:00 -0500

    (server) use body parser

--- trust-pool-app/server/server.js
+++ trust-pool-app/server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = http.Server(app);
 app.use(cors());
+app.use(bodyParser.json());
+
 
 module.exports.server = server;
