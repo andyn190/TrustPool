ff47ef2230b02343832f991df4a60258a2b38512
Jelani <jhankins02@gmail.com>
2018-06-25 13:55:54 -0500

    (add) get specific pool to poolsRouter

--- trust-pool-app/server/routers/pools.js
+++ trust-pool-app/server/routers/pools.js
@@ -4,7 +4,14 @@ const pools = express.Router();
 
 pools.get('/', (req, res) => {
   res.status(200).send('sent get request to pools');
+  // this will respond with all public pools
 });
+
+pools.get('/:poolId', (req, res) => {
+  res.status(200).send(`sent request to get pool ${req.params.poolId}`);
+  // this will respond with the pool requested
+});
+
 pools.post('/', (req, res) => {
   res.status(200).send('sent post request to pools');
 });
