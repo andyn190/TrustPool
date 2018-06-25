0d0ca19516fb1d7bf6ca9b947a699f6445797ff9
Jelani <jhankins02@gmail.com>
2018-06-25 14:26:29 -0500

    (add) post to signup vars

--- trust-pool-app/server/routers/signup.js
+++ trust-pool-app/server/routers/signup.js
@@ -7,7 +7,8 @@ signup.get('/', (req, res) => {
 });
 
 signup.post('/', (req, res) => {
-  res.status(200).send('recieved post request to signup');
+  const {firstName, lastName, email, password, imgUrl} = req.body;
+  res.status(200).send(`recieved post request to signup user ${lastName}, ${firstName}`);
 });
 
 module.exports = signup;
