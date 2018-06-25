e0068c928b7ede17272f903d90e9e7c6b9d681fb
Jelani <jhankins02@gmail.com>
2018-06-25 16:55:10 -0500

    (add) loginRoute get '/', '/google', 'google/redirect'

--- trust-pool-app/server/routers/login.js
+++ trust-pool-app/server/routers/login.js
@@ -1,11 +1,19 @@
 const login = require('express').Router();
+const passport = require('passport');
 
 login.post('/', (req, res) => {
   res.status(200).send('recieved post request to login');
 });
 
-login.post('/google', (req, res) => {
-  res.status(200).send('recieved post request to login with google');
-});
+login.get('/google/redirect', passport.authenticate('google', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+}));
+
+login.get('/', (req, res) => res.send({ user: req.user || null }));
+
+login.get('/google', passport.authenticate('google', {
+  scope: ['profile'],
+}));
 
 module.exports = login;
