96ce80901ef4be9c245bd52f470640fc0b0dc6ae
Jelani <jhankins02@gmail.com>
2018-06-25 12:37:55 -0500

    (add) set up routers, use dist path

--- trust-pool-app/server/server.js
+++ trust-pool-app/server/server.js
@@ -3,10 +3,15 @@ const http = require('http');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const setupRouters = require('./routers');
+const { PUBLIC_PATH } = require('./config');
+
 const app = express();
 const server = http.Server(app);
 app.use(cors());
 app.use(bodyParser.json());
 
+setupRouters(app);
 
+app.use(express.static(PUBLIC_PATH));
 module.exports.server = server;
