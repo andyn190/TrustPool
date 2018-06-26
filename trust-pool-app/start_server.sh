f6a24e534a37173e75560cd57b6efd29dc30b877
Jelani <jhankins02@gmail.com>
2018-06-26 16:06:56 -0500

    (add) port 80

--- trust-pool-app/start_server.sh
+++ trust-pool-app/start_server.sh
@@ -3,6 +3,8 @@ cd /home/ec2-user/trustpoolapp/trust-pool-app
 export GOOGLE_CLIENT_ID=87303288866-1a0i47h32fjal3gb8nat484mvegfs0ge.apps.googleusercontent.com
 export GOOGLE_CLIENT_SECRET=5lXvwP99trSb0RBEKQ9pWjpk
 export AWSPASSWORD=admin123
+export PORT=80
+
 sudo chmod 777 .
 npm install && npm run build
 npm start
