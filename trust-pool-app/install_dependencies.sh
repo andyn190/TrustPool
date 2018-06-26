f72e96efad2ef2e02b4e0708e4075946a5a624f9
Jelani <jhankins02@gmail.com>
2018-06-26 13:37:18 -0500

    (install) node

--- trust-pool-app/install_dependencies.sh
+++ trust-pool-app/install_dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo chmod 777 .
+yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
+yum install nodejs
 npm -v && node -v
 npm install && npm run build
