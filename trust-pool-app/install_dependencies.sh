c05f080b3ca45ca64a1022062147af52862356cb
Jelani <jhankins02@gmail.com>
2018-06-26 13:57:11 -0500

    isNodeInstalled

--- trust-pool-app/install_dependencies.sh
+++ trust-pool-app/install_dependencies.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo chmod 777 .
-sudo yum install gcc-c++ make
-curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
-sudo yum -y install nodejs
+
 npm -v && node -v
 npm install && npm run build
