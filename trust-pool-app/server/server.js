822ba74059a11e36fa20bb1a01c2454cc8957f40
Jelani <jhankins02@gmail.com>
2018-06-27 10:09:33 -0500

    (rename) db.js >

--- trust-pool-app/server/server.js
+++ trust-pool-app/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const path = require('path');
-const db = require(path.join('${__dirname}', './../../database/db.js'))
+const db = require(path.join('${__dirname}', './../../database'))
 const bodyParser = require('body-parser');
 
 const setupRouters = require('./routers');
