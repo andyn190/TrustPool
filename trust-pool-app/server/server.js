8b60adb0b54f8d1f9f96eff62e664af2191f5c75
Jelani <jhankins02@gmail.com>
2018-06-27 15:37:01 -0500

    (clean-up)

--- trust-pool-app/server/server.js
+++ trust-pool-app/server/server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const path = require('path');
-const db = require(path.join('${__dirname}', './../../database'));
-const helpers = require(path.join('${__dirname}', './../../database/helpers.js'));
 const bodyParser = require('body-parser');
+
+const db = require(path.join('${__dirname}', './../../database'));
+const {
+  findOrCreate,
+  findOrCreateUser,
+} = require(path.join('${__dirname}', './../../database/helpers.js'));
 
 const setupRouters = require('./routers');
 const setupPassport = require('./passport');
