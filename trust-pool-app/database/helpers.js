76a6c1932e6b40834286bf6fdb2aa14f2ebab06e
Jelani <jhankins02@gmail.com>
2018-06-27 12:29:26 -0500

    (create) helper, findOrCreate

--- trust-pool-app/database/helpers.js
+++ trust-pool-app/database/helpers.js
@@ -22,10 +22,23 @@ const models = {
 }
 
 const findOrCreate = ( model, where ) => {
-  models[model].findOrCreate(where).spread((user, created) => {
-    console.log(user.get({
+  return new Promise((resolve, reject) => {
+    models[model].findOrCreate(where).spread((result, created) => {
+    const item = result.get({
       plain: true
-    }))
+    });
+    console.log(item);
+    if(item){
+      resolve(item);
+    } else {
+      reject();
+    }
     console.log(created)
-  })
+  });
+  });
 }
+
+findOrCreate('Users', { where: {email: 'jhankins02@gmail.com'}, defaults: {'first_name': 'jelani', 'last_name': 'hankins', 'image_url': 'https://lh6.googleusercontent.com/-xVfxeB5daxA/AAAAAAAAAAI/AAAAAAAAAkc/sHx0_aCUNnU/photo.jpg?sz=50', password: 'password' } })
+.then((user) => {console.log(user)})
+.catch((err) => { console.log(err);});
+
