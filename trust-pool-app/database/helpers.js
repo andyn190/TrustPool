512044bc4aa0517aaeee4f81d180ea710f92737a
Jelani <jhankins02@gmail.com>
2018-06-27 10:49:06 -0500

    (add) models obj, findOrCreate

--- trust-pool-app/database/helpers.js
+++ trust-pool-app/database/helpers.js
@@ -8,3 +8,24 @@ PoolMembers,
 ChatMessages,
 EbayWishlistEntry,
 Checks } = require('.');
+
+const models = {
+  Users,
+  Pools,
+  ExpenseRequestType,
+  ExpenseRequest,
+  ContributionEntry,
+  PoolMembers,
+  ChatMessages,
+  EbayWishlistEntry,
+  Checks
+}
+
+const findOrCreate = ( model, where ) => {
+  models[model].findOrCreate(where).spread((user, created) => {
+    console.log(user.get({
+      plain: true
+    }))
+    console.log(created)
+  })
+}
