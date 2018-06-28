afcf7e67288c34a99cd25ee221c6f4c343862823
Jelani <jhankins02@gmail.com>
2018-06-27 20:08:48 -0500

    (add) export passport, (clean) promises, use order

--- trust-pool-app/server/passport/index.js
+++ trust-pool-app/server/passport/index.js
@@ -8,16 +8,12 @@ const {
   findUserById,
 } = require('./../../database/helpers.js');
 
-module.exports = (app) => {
-  app.use(session(SESSION_OPTS));
-  app.use(passport.initialize());
-  app.use(passport.session());
-
+module.exports = { setupPassport: (app) => {
   passport.serializeUser((user, done) => done(null, user.id));
   passport.deserializeUser((id, done) => {
     findUserById(id)
-      .then(user=>done(null, user))
-      .catch(err=>console.log(err));
+      .then(user => done(null, user))
+      .catch(err => done(err));
   });
 
   passport.use(new GoogleStrategy({
@@ -25,17 +21,17 @@ module.exports = (app) => {
     clientID: GOOGLE_KEYS.CLIENT_ID,
     clientSecret: GOOGLE_KEYS.CLIENT_SECRET,
   }, (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken, 'Access Token');
-    console.log(refreshToken, 'Refresh Token');
-    console.log(profile, 'Profile');
     const { name, _json, id} = profile; 
     const last_name = name.familyName;
     const first_name = name.givenName;
     const image_url = _json.image.url;
-    console.log(image_url);
     findOrCreateUser(null, first_name, last_name, image_url, null, id)
-      .then((user) => {done(null, user);})
-      .catch((err) => { done(err, null);});
-    
+      .then(user => done(null, user))
+      .catch(err =>  done(err));
   }));
+
+  app.use(session(SESSION_OPTS));
+  app.use(passport.initialize());
+  app.use(passport.session());
+}, passport,
 };
