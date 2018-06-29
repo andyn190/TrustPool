eac256ecfd980e5d1bc1735d79ee269478bf48d5
Jelani <jhankins02@gmail.com>
2018-06-28 19:58:52 -0500

    (add) get pools http get '/pools'

--- trust-pool-app/client/app/services/pools/pools.service.ts
+++ trust-pool-app/client/app/services/pools/pools.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoolsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
   getPools() {
     return this.http.get('/pools');
   }
