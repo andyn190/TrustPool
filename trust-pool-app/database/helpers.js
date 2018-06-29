61e83f4068505f18f0ea70c94d6d6c69d69f4e8c
Jelani <jhankins02@gmail.com>
2018-06-29 16:06:11 -0500

    (add) findPoolbyID updateMemberCount

--- trust-pool-app/database/helpers.js
+++ trust-pool-app/database/helpers.js
@@ -9,127 +9,145 @@ const { sequelize,
   EbayWishlistEntry,
   Checks } = require('.');
   
-  const models = {
-    Users,
-    Pools,
-    ExpenseRequestType,
-    ExpenseRequest,
-    ContributionEntry,
-    PoolMembers,
-    ChatMessages,
-    EbayWishlistEntry,
-    Checks
-  };
-  
-  const findOne = ( model, where ) => {
+const models = {
+  Users,
+  Pools,
+  ExpenseRequestType,
+  ExpenseRequest,
+  ContributionEntry,
+  PoolMembers,
+  ChatMessages,
+  EbayWishlistEntry,
+  Checks
+};
+
+const findOne = ( model, where ) => {
+  return new Promise((resolve, reject) => {
+    models[model].find(where)
+    .then((item) => {
+      resolve(item);
+    })
+    .catch((err)=>{
+      reject(err);
+    });
+  });
+};
+
+const findUserById = ( id ) => {
+    return findOne('Users', { where: { id } });
+};
+
+const findUserByGoogle = (googleID) => {
+return findOne('Users', { where: { googleID } });
+};
+
+const findPoolByName = ( name ) => {
+    return findOne('Pools', { where: { name } });
+};
+
+const findPoolById = (id) => {
+  return findOne('Pools', { where: { id } });
+};
+
+const findAll = ( model, where ) => {
+  if(where) {
     return new Promise((resolve, reject) => {
-      models[model].find(where)
+      models[model].findAll(where)
       .then((item) => {
         resolve(item);
       })
       .catch((err)=>{
         reject(err);
       });
-    });
-  };
-  
-  const findUserById = ( id ) => {
-      return findOne('Users', { where: { id } });
-  };
-
-const findUserByGoogle = (googleID) => {
-  return findOne('Users', { where: { googleID } });
+    }); 
+  } else {
+    return new Promise((resolve, reject) => {
+      models[model].findAll()
+      .then((item) => {
+        resolve(item);
+      })
+      .catch((err)=>{
+        reject(err);
+      });
+    }); 
+  }
 };
-  
-  const findPoolByName = ( name ) => {
-      return findOne('Pools', { where: { name } });
-  };
-  
-  const findAll = ( model, where ) => {
-    if(where) {
-      return new Promise((resolve, reject) => {
-        models[model].findAll(where)
-        .then((item) => {
-          resolve(item);
-        })
-        .catch((err)=>{
-          reject(err);
-        });
-      }); 
-    } else {
-      return new Promise((resolve, reject) => {
-        models[model].findAll()
-        .then((item) => {
-          resolve(item);
-        })
-        .catch((err)=>{
-          reject(err);
-        });
-      }); 
-    }
-  };
-  
-  const findAllPools = () => {
-      return findAll('Pools');
-  };
+
+const findAllPools = () => {
+    return findAll('Pools');
+};
+
 const findAllPoolMembers = (pool_id) => {
   return findAll('PoolMembers', { where: { pool_id } });
 };
-  
-  const findOrCreate = ( model, where ) => {
-    return new Promise((resolve, reject) => {
-      models[model].findOrCreate(where).spread((result, created) => {
-      const item = result.get({
-        plain: true
-      });
-      item.isNewRecord = result.isNewRecord;
-      if(item){
-        resolve(item);
-      } else {
-        reject();
-      }
-    });
-    });
-  };
-  
-  const findOrCreateUser = (email, first_name, last_name, image_url, password, googleID) => {
-    if(email){
-      return findOrCreate('Users', { where: {email}, defaults: {first_name, last_name, image_url, password } });
-    } 
-    if( googleID ) {
-      return findOrCreate('Users', { where: { googleID }, defaults: {first_name, last_name, image_url, password , googleID} });
-    } 
-  };
-  
-  const create = (model, item) => {
-    return models[model].create(item);
-      User.create({ username: 'fnord', job: 'omnomnom' })
-      return findOrCreate('Pools', { where: { name }, defaults: { imgUrl, desc, voteConfig, creator, public }});
-  };
-  
-  const createPool = (name, imageURL, description, voteConfig, creator, public) => {
-    const pool = { name, imageURL, description, voteConfig, creator, public, pool_value: 0, members_count: 0 };
-    return create('Pools', pool);
-  }
 
-  const createPoolMember = (pool_id, pool_member_id) => {
-    const contrubution_amount = 0, withdraw_amount = 0;
-    const poolMember = { pool_id, pool_member_id, contrubution_amount, withdraw_amount  };
-    return create('PoolMembers', poolMember);
-  }
-  
-  module.exports = {
-    findOrCreate,
-    findOrCreateUser,
-    findOne,
-    findUserById,
-    create,
-    createPool,
-    findPoolByName,
-    findAllPools,
-    findAll,
-    createPoolMember,
-    findUserByGoogle,
-    findAllPoolMembers
-  };
+const findOrCreate = ( model, where ) => {
+  return new Promise((resolve, reject) => {
+    models[model].findOrCreate(where).spread((result, created) => {
+    const item = result.get({
+      plain: true
+    });
+    item.isNewRecord = result.isNewRecord;
+    if(item){
+      resolve(item);
+    } else {
+      reject();
+    }
+  });
+  });
+};
+
+const findOrCreateUser = (email, first_name, last_name, image_url, password, googleID) => {
+  if(email){
+    return findOrCreate('Users', { where: {email}, defaults: {first_name, last_name, image_url, password } });
+  } 
+  if( googleID ) {
+    return findOrCreate('Users', { where: { googleID }, defaults: {first_name, last_name, image_url, password , googleID} });
+  } 
+};
+
+const create = (model, item) => {
+  return models[model].create(item);
+    User.create({ username: 'fnord', job: 'omnomnom' })
+    return findOrCreate('Pools', { where: { name }, defaults: { imgUrl, desc, voteConfig, creator, public }});
+};
+
+const createPool = (name, imageURL, description, voteConfig, creator, public) => {
+  const pool = { name, imageURL, description, voteConfig, creator, public, pool_value: 0, members_count: 0 };
+  return create('Pools', pool);
+};
+
+const createPoolMember = (pool_id, pool_member_id) => {
+  const contrubution_amount = 0, withdraw_amount = 0;
+  const poolMember = { pool_id, pool_member_id, contrubution_amount, withdraw_amount  };
+  return create('PoolMembers', poolMember);
+};
+
+const updateMemberCount = (id, amount) => {
+  findPoolById(id)
+    .then((pool) => {
+      let { members_count } = pool;
+      pool.members_count = members_count + amount;
+      pool.save()
+      .then((update) => { console.log('POOL MEMBERS COUNT UPDATED')})
+        .catch((err) => { console.log('POOL MEMBERS COUNT NOT UPDATED', err)});
+    })
+    .catch((err) => { console.log('FAILED TO FIND POOL BY ID', err)});
+};
+
+module.exports = {
+  findOrCreate,
+  findOrCreateUser,
+  findOne,
+  findUserById,
+  create,
+  createPool,
+  findPoolByName,
+  findAllPools,
+  findAll,
+  createPoolMember,
+  findUserByGoogle,
+  findAllPoolMembers,
+  updateMemberCount
+};
   
