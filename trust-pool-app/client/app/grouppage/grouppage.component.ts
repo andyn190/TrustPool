7cbe4de0e03497a3d1925228674d9d60c604a402
Jelani <jhankins02@gmail.com>
2018-06-30 11:20:09 -0500

    (remove) ngx stripe

--- trust-pool-app/client/app/grouppage/grouppage.component.ts
+++ trust-pool-app/client/app/grouppage/grouppage.component.ts
@@ -3,7 +3,6 @@ import { CookieService } from 'angular2-cookie/core';
 import { PoolsService } from '../services/pools/pools.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { StripeService, Elements, Element as StripeElement, ElementsOptions } from "ngx-stripe";
 
 @Component({
   selector: 'app-grouppage',
@@ -11,54 +10,20 @@ import { StripeService, Elements, Element as StripeElement, ElementsOptions } fr
   styleUrls: ['./grouppage.component.css']
 })
 export class GrouppageComponent implements OnInit {
-  elements: Elements;
-  card: StripeElement;
   poolid: number;
   isMember: boolean;
   pool: object;
   private sub: any;
 
-  elementsOptions: ElementsOptions = {
-    locale: 'es'
-  };
   contribute: FormGroup;
   constructor(
     private _poolsService: PoolsService,
     private _cookieService: CookieService,
     private _router: Router,
     private route: ActivatedRoute,
-    private fb: FormBuilder,
-    private stripeService: StripeService
   ) { }
 
   ngOnInit() {
-    this.contribute = this.fb.group({
-      name: ['', [Validators.required]]
-    });
-    this.stripeService.elements(this.elementsOptions)
-      .subscribe(elements => {
-        this.elements = elements;
-        // Only mount the element the first time
-        if (!this.card) {
-          this.card = this.elements.create('card', {
-            style: {
-              base: {
-                iconColor: '#666EE8',
-                color: '#31325F',
-                lineHeight: '40px',
-                fontWeight: 300,
-                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-                fontSize: '18px',
-                '::placeholder': {
-                  color: '#CFD7E0'
-                }
-              }
-            }
-          });
-          this.card.mount('#card-element');
-        }
-      });
-    
     this.sub = this.route.params.subscribe(params => {
       let { poolid, isMember, getPool, checkIsMember  } = this;
       poolid = +params['poolid']; // (+) converts string 'id' to a number
@@ -70,21 +35,6 @@ export class GrouppageComponent implements OnInit {
   }
   viewGroups() {
     this._router.navigate(['groups']);
-  }
-  buy() {
-    const name = this.contribute.get('name').value;
-    this.stripeService
-      .createToken(this.card, { name })
-      .subscribe(result => {
-        if (result.token) {
-          // Use the token to create a charge or a customer
-          // https://stripe.com/docs/charges
-          console.log(result.token);
-        } else if (result.error) {
-          // Error creating the token
-          console.log(result.error.message);
-        }
-      });
   }
   getPool(poolid) {
     this._poolsService.getPool(poolid).subscribe(
