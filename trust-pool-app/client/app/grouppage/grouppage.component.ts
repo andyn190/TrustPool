88c1d06110dd15d0de32ff0f76ee5be77a29f406
Jelani <jhankins02@gmail.com>
2018-06-29 19:45:55 -0500

    (add) viewGroups route back to groups

--- trust-pool-app/client/app/grouppage/grouppage.component.ts
+++ trust-pool-app/client/app/grouppage/grouppage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'angular2-cookie/core';
+import { PoolsService } from '../services/pools/pools.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-grouppage',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GrouppageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _poolsService: PoolsService, private _cookieService: CookieService, private _router: Router) { }
 
   ngOnInit() {
+  }
+  viewGroups() {
+    this._router.navigate(['groups']);
   }
 
 }
