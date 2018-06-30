e5d604a8ae4ca90f078e7b3ca58cbcc1627e5854
Jelani <jhankins02@gmail.com>
2018-06-30 11:08:31 -0500

    (add)ngOnInit ngx stripe

--- trust-pool-app/client/app/grouppage/grouppage.component.ts
+++ trust-pool-app/client/app/grouppage/grouppage.component.ts
@@ -32,6 +32,33 @@ export class GrouppageComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.contribute = this.fb.group({
+      name: ['', [Validators.required]]
+    });
+    this.stripeService.elements(this.elementsOptions)
+      .subscribe(elements => {
+        this.elements = elements;
+        // Only mount the element the first time
+        if (!this.card) {
+          this.card = this.elements.create('card', {
+            style: {
+              base: {
+                iconColor: '#666EE8',
+                color: '#31325F',
+                lineHeight: '40px',
+                fontWeight: 300,
+                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                fontSize: '18px',
+                '::placeholder': {
+                  color: '#CFD7E0'
+                }
+              }
+            }
+          });
+          this.card.mount('#card-element');
+        }
+      });
+    
     this.sub = this.route.params.subscribe(params => {
       let { poolid, isMember, getPool, checkIsMember  } = this;
       poolid = +params['poolid']; // (+) converts string 'id' to a number
@@ -43,6 +70,21 @@ export class GrouppageComponent implements OnInit {
   }
   viewGroups() {
     this._router.navigate(['groups']);
+  }
+  buy() {
+    const name = this.contribute.get('name').value;
+    this.stripeService
+      .createToken(this.card, { name })
+      .subscribe(result => {
+        if (result.token) {
+          // Use the token to create a charge or a customer
+          // https://stripe.com/docs/charges
+          console.log(result.token);
+        } else if (result.error) {
+          // Error creating the token
+          console.log(result.error.message);
+        }
+      });
   }
   getPool(poolid) {
     this._poolsService.getPool(poolid).subscribe(
