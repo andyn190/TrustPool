2fd9fd742b2e0784e42e71c676a8bc307184b0a0
Jelani <jhankins02@gmail.com>
2018-07-01 16:09:38 -0500

    (aad) http setup

--- trust-pool-app/client/app/services/user/user.service.ts
+++ trust-pool-app/client/app/services/user/user.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { 
+  constructor(private http: HttpClient) { 
     console.log('USER SERVICE INIT!!!!!');
   }
 
