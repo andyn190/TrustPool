e529475ae880af1e0ea6894597abfeb68715f1dc
Jelani <jhankins02@gmail.com>
2018-07-01 16:15:06 -0500

    (add) use  user router

--- trust-pool-app/server/routers/index.js
+++ trust-pool-app/server/routers/index.js
@@ -2,10 +2,12 @@ const poolsRouter = require('./pools');
 const signupRouter = require('./signup');
 const loginRouter = require('./login');
 const logoutRouter = require('./logout');
+const userRouter = require('./user');
 
 module.exports = (app) => {
   app.use('/pools', poolsRouter);
   app.use('/signup', signupRouter);
   app.use('/login', loginRouter);
   app.use('/logout', logoutRouter);
+  app.use('/user', userRouter);
 };
