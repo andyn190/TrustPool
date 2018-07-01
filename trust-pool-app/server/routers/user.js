f4b8a38233bff456d97325da5ad1f54ceab9d1e4
Jelani <jhankins02@gmail.com>
2018-07-01 16:26:23 -0500

    (add) finduser by google id

--- trust-pool-app/server/routers/user.js
+++ trust-pool-app/server/routers/user.js
@@ -1,9 +1,12 @@
 const user = require('express').Router();
+const { findUserByGoogle } = require('./../../database/helpers');
 
 user.get('/', (req, res) => {
-  
-  res.status(200).send('recieved get request to log out');
+  const userCookie = req.user;
+  const { googleID } = userCookie;
+  findUserByGoogle(googleID)
+    .then(resUser => res.status(200).json({ user: resUser }))
+    .catch(err => res.status(200).json({ err }));
 });
-
 
 module.exports = user;
