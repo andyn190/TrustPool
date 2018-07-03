8250bd8e1f689a21d76a4fe43fa59e9af6b4850a
Jelani <jhankins02@gmail.com>
2018-07-03 15:45:30 -0500

    (add) procces env

--- trust-pool-app/server/config.js
+++ trust-pool-app/server/config.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   STRIPEKEY: process.env.STRIPEKEYTEST || process.env.STRIPEKEYLIVE,
   MAILGUN: {
-    domain: 'sandboxa79442dcbad44327aeccdaabfe65e452.mailgun.org',
-    apiKey: '3811e1a959c45546249d7195db05eadc-770f03c4-71135289'
+    domain: process.env.MAILDOMAIN,
+    apiKey: process.env.MAILKEY
   }
 };
