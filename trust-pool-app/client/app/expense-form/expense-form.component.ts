ecfceebcc1c13cc55b1df23629b3dcc3334cc78f
Andy Nguyen <andyn190@gmail.com>
2018-07-04 15:07:51 -0500

    made form for expense requests

--- trust-pool-app/client/app/expense-form/expense-form.component.ts
+++ trust-pool-app/client/app/expense-form/expense-form.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, Routes } from '@angular/router';
+import { Component, OnInit, Optional } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, NgForm } from "@angular/forms";
+import { OwnAuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-expense-form',
   templateUrl: './expense-form.component.html',
   styleUrls: ['./expense-form.component.css']
 })
+@Optional()
 export class ExpenseFormComponent implements OnInit {
   constructor(
     private _router: Router,
-    private routes: Routes,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private auth: OwnAuthService
   ) { }
   public poolid : number
   private sub : any
+  private user: any
 
   ngOnInit() {
     this.route.queryParams
       .subscribe(params => {
         this.poolid = params.poolid;
-      })
+      });
+    this.auth.checkLogin().subscribe(({ user }: any) => {
+      this.user = user;
+    })
   }
 
 }
