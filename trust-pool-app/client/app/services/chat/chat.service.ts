b29d929cf900ef583e2d46827106263dda78eb8f
Jelani <jhankins02@gmail.com>
2018-07-09 16:37:30 -0500

    (add) joinRequestChat, newUserJoined

--- trust-pool-app/client/app/services/chat/chat.service.ts
+++ trust-pool-app/client/app/services/chat/chat.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-
+  
+  private socket = io("http://localhost:8080");
   constructor() { }
+
+
+  joinRequestChat(data) {
+    this.socket.emit('join', data);
+  }
+
+  newUserJoined(){
+    let observable = new Observable < { userName: String, message: String }>(observer =>{
+      this.socket.on('userHasJoined', (data) => {
+        observer.next(data);
+      });
+      return () => this.socket.disconnect();
+    });
+    return observable;
+  }
+
 }
