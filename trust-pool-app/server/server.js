025a59744e7be8acea63a8f68802142692aa52f5
Jelani <jhankins02@gmail.com>
2018-07-09 16:38:49 -0500

    (add) socket io listen on connection

--- trust-pool-app/server/server.js
+++ trust-pool-app/server/server.js
@@ -4,16 +4,23 @@ const cors = require('cors');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+let io = require('socket.io');
 
 const db = require(path.join('${__dirname}', './../../database'));
 
 const setupRouters = require('./routers');
 const { setupPassport } = require('./passport');
+const setupListeners = require('./sockets');
 const authenticated = require('./passport/authenticated');
 const { PUBLIC_PATH } = require('./config');
 
 const app = express();
 const server = http.Server(app);
+io = io.listen(server);
+io.on('connection', (socket) => {
+  console.log('NEW CONNECTION MADE');
+  setupListeners(socket);
+});
 app.use(cors());
 app.use(require('cookie-parser')());
 
