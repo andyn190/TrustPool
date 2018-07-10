a00f4fa56094d962ca67b551582158649d1547f3
Elivasquez <elivasquez646@gmail.com>
2018-07-09 20:12:52 -0500

    Route to get a user by id

--- trust-pool-app/server/routers/user.js
+++ trust-pool-app/server/routers/user.js
@@ -19,4 +19,12 @@ user.post('/update', (req, res) => {
     .catch(err => res.send(500).json({ err }));
 });
 
+user.get('/:id', (req, res) => {
+  const { params } = req;
+  const { id } = params;
+  findUserById(id)
+    .then(resUser => res.status(200).json({ user: resUser }))
+    .catch(err => res.status(404).json({ err }));
+});
+
 module.exports = user;
