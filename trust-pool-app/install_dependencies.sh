c3dbb8a9b3fd005c6cd255cb0f7e1074add92942
Jelani <jhankins02@gmail.com>
2018-07-11 15:26:39 -0500

    (add) update npm

--- trust-pool-app/install_dependencies.sh
+++ trust-pool-app/install_dependencies.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-cd /home/ec2-user
+cd /home/ec2-user/trustpoolapp/trust-pool-app
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh
 . ~/.nvm/nvm.sh
-npm install -g npm@latest
+sudo npm install -g npm@latest
 nvm install --lts
 nvm use 8
 
-cd /trustpoolapp/trust-pool-app
 sudo chmod 777 .
 sudo npm install && npm run build
