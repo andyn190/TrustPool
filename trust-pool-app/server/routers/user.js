dcccafe4fd9cffec40b22822403c6c12a6cf4a06
Elivasquez <elivasquez646@gmail.com>
2018-07-10 22:40:16 -0500

    User update route able to handle user photoid upload and admin verify

--- trust-pool-app/server/routers/user.js
+++ trust-pool-app/server/routers/user.js
@@ -1,7 +1,11 @@
 const user = require('express').Router();
+const cloudinary = require('cloudinary');
 const { findUserByGoogle } = require('./../../database/helpers');
 const { findUserByGoogleAndUpdate } = require('./../../database/helpers');
 const { findUserById } = require('./../../database/helpers');
+const { CLOUDINARY } = require('../config');
+const { ADMIN } = require('../config');
+
 
 user.get('/', (req, res) => {
   const userCookie = req.user;
@@ -13,14 +17,41 @@ user.get('/', (req, res) => {
 
 user.post('/update', (req, res) => {
   const { body, user } = req;
-  findUserByGoogleAndUpdate(user.googleID, body)
-    .then((updatedUser) => {
-      res.status(200).send(updatedUser);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
+  const adminVerify = ADMIN.admin_password;
+  if (body.admin === adminVerify) {
+    body.admin = true;
+  } else {
+    body.admin = false;
+  }
+  const options = {
+    tag: body.name,
+    api_key: CLOUDINARY.api_key,
+    cloud_name: CLOUDINARY.cloud_name,
+    api_secret: CLOUDINARY.api_secret
+  };
+  if (body.photoID) {
+    cloudinary.v2.uploader.upload(body.photoID, options, (err, response) => {
+      if (err) {
+        return res.status(400).json({ err, type: 'CLOUD' });
+      }
+      body.photoID = response.url;
+      findUserByGoogleAndUpdate(user.googleID, body)
+        .then((updatedUser) => {
+          res.status(200).send(updatedUser);
+        })
+        .catch((error) => {
+          res.status(400).send(error);
+        });
     });
-  // console.log(user);
+  } else {
+    findUserByGoogleAndUpdate(user.googleID, body)
+      .then((updatedUser) => {
+        res.status(200).send(updatedUser);
+      })
+      .catch((error) => {
+        res.status(400).send(error);
+      });
+  }
 });
 
 user.get('/:id', (req, res) => {
