3167b4728ddbc67e72968de7a05b2ad6504c0313
Elivasquez <elivasquez646@gmail.com>
2018-07-10 22:41:21 -0500

    Enable admin verify with password specified in the process .env

--- trust-pool-app/server/config.js
+++ trust-pool-app/server/config.js
@@ -27,5 +27,8 @@ module.exports = {
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
+  },
+  ADMIN: {
+    admin_password: process.env.ADMIN
   }
 };
