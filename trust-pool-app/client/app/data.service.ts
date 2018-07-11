7aa8d28336a8f0329aeee7021f7b6bfc573bb2e2
Andy Nguyen <andyn190@gmail.com>
2018-07-10 20:45:44 -0500

    data service to maintain user status

--- trust-pool-app/client/app/data.service.ts
+++ trust-pool-app/client/app/data.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { OwnAuthService } from './services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  private loggedIn = new Subject<any>();
-  constructor() { }
-  getData(): Observable<any> {
-    return this.loggedIn.asObservable();
+  private loggedIn = new BehaviorSubject<boolean>(false);
+  loggedIn$ = this.loggedIn.asObservable();
+  constructor(private auth: OwnAuthService) { }
+
+  userLogin(userStatus: boolean) {
+    this.loggedIn.value = userStatus;
+  }
+  userLogout() {
+
   }
 }
